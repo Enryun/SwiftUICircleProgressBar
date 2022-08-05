d8545a7f9a6af3ffc7a0a50a09e5811f91a372dd
JamesThang996 <thang.duong@kardiachain.io>
2022-08-05 22:56:05 +0700

    Initial Commit

---
+++ SwiftUICircleProgressBar/SwiftUICircleProgressBarApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUICircleProgressBarApp.swift
+//  SwiftUICircleProgressBar
+//
+//  Created by James Thang on 05/08/2022.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUICircleProgressBarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
